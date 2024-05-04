c900d37acc11cc5c51c6c0d11279d1582b8aee2b
DESKTOP-6NEOP34\SAMSUNG <ehdejdrnfl@naver.com>
2024-05-04 11:39:55 +0900

    event

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './pages/Main'; 
 import Layout from './layout/layout';
+import Eventpage from "./pages/Eventpage";
+import Nav from "./component/Nav";
 
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* Assuming you want Main to be the default content of Layout */}
           <Route index element={<Main />} />
           {/* You can add more nested routes if necessary */}
+          
+          <Route path="event" element={<Eventpage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
